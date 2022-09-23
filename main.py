c764d976cb77a06fdef770106044fd2b1a4fcb51
RenatBogdanov <zeloniyosminog@gmail.com>
2022-09-23 16:11:10 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+
+url = 'https://dtf.ru/'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')
+quotes = soup.find_all('div', class_='content-title content-title--short l-island-a')
+
+print(requests.get(url).status_code)
+
+for quote in quotes:
+
+	print(quote.text.replace("Статьи редакции", '').strip(' \n\t'))
+	#time.sleep(3)
+
+print("-----------\nE N D")
+
+
